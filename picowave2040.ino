cb6887109e20068dc3b93c0663dc50591f96112a
Jeremy Arnold <jeremyarnold@Jeremys-MacBook-Pro.local>
2023-05-25 22:36:07 +1200

    4 voice poly!

--- picowave2040.ino
+++ picowave2040.ino
@@ -17,7 +17,8 @@ Adafruit_USBD_MIDI usb_midi;
 // and attach usb_midi as the transport.
 MIDI_CREATE_INSTANCE(Adafruit_USBD_MIDI, usb_midi, MIDI);
 
-PWMAudio pwm(0, true); // GP0 = left, GP1 = right
+PWMAudio pwm_channel1(0, true); // GP0 = left, GP1 = right
+PWMAudio pwm_channel2(2, true); // GP0 = left, GP1 = right
 
 //const int freq = 48000; // Output frequency for PWM
 const int freq = 32470; //12bit - 133mhz / 4096
@@ -27,8 +28,9 @@ const uint8_t N = 7; //Number of bits in wavetable
 static filter1pole filter;
 static int8_t filter_cutoff = 63;
 
-int16_t al = 0;
-uint8_t modulation = 13;
+unsigned int lfo = 0;
+int16_t modulation = 0;
+uint8_t modulation_step = 13;
 
 uint8_t wta_sel = 0;
 uint8_t wtb_sel = 28;
@@ -45,14 +47,19 @@ typedef struct {
   uint8_t waveA;
   uint8_t waveB;
   bool gate = false;
+  uint8_t amp;
   uint8_t note;
   int freq;
   uint32_t phase_step = 0;
   uint32_t phase_accum = 0x00000000;
   int16_t output;
-} osc;
+} voice_state;
 
-osc voices[8];
+voice_state voices[8];
+
+uint8_t voice_alloc_head = 0;
+uint8_t voice_alloc_tail = 1;
+uint8_t voice_allocation[8] = { 0, 1, 2, 3 };
 
 uint8_t  address_pointer = 0x00;
 
@@ -60,35 +67,55 @@ uint8_t sineTable[128]; // Precompute sine wave in 128 steps
 uint8_t wavetableA[128]; // Precompute sine wave in 128 steps
 uint8_t wavetableB[128]; // Precompute sine wave in 128 steps
 
-unsigned int cnt = 0;
-unsigned int lfo = 0;
-uint8_t eg = 0;
-int16_t output = 0;
-void cb() {
-  al += modulation;
-  while (pwm.availableForWrite()) {
-    lfo = sineTable[((al>>8)&127)];
-    osc *voiceA = &voices[0];
+void channel1_cb() {
+  modulation += modulation_step;
+  while (pwm_channel1.availableForWrite()) {
+    lfo = sineTable[((modulation>>8)&127)];
+    voice_state *voiceA = &voices[0];
     voiceA->phase_accum += voiceA->phase_step;
     // 32 accumulator bits - 7 wavetable bits
     uint8_t phaseA = voiceA->phase_accum >> (32-N);
     voiceA->output = ((wavetableA[phaseA]) * lfo);
     voiceA->output += ((wavetableB[phaseA]) * (127-lfo));
     //output = filter1pole_feed(&filter, (eg>>4), output);
-    voiceA->output = (voiceA->output>>8) * (eg);
+    voiceA->output = (voiceA->output>>8) * (voiceA->amp);
 
-    osc *voiceB = &voices[1];
+    voice_state *voiceB = &voices[1];
     voiceB->phase_accum += voiceB->phase_step;
     // 32 accumulator bits - 7 wavetable bits
     uint8_t phaseB = voiceB->phase_accum >> (32-N);
     voiceB->output = ((wavetableA[phaseB]) * lfo);
     voiceB->output += ((wavetableB[phaseB]) * (127-lfo));
     //output = filter1pole_feed(&filter, (eg>>4), output);
-    voiceB->output = (voiceB->output>>8) * (eg);
+    voiceB->output = (voiceB->output>>8) * (voiceB->amp);
 
-    pwm.write(voiceA->output);
-    pwm.write(voiceB->output);
-    cnt++;
+    pwm_channel1.write(voiceA->output);
+    pwm_channel1.write(voiceB->output);
+  }
+}
+
+void channel2_cb() {
+  while (pwm_channel2.availableForWrite()) {
+    voice_state *voiceA = &voices[2];
+    voiceA->phase_accum += voiceA->phase_step;
+    // 32 accumulator bits - 7 wavetable bits
+    uint8_t phaseA = voiceA->phase_accum >> (32-N);
+    voiceA->output = ((wavetableA[phaseA]) * lfo);
+    voiceA->output += ((wavetableB[phaseA]) * (127-lfo));
+    //output = filter1pole_feed(&filter, (eg>>4), output);
+    voiceA->output = (voiceA->output>>8) * (voiceA->amp);
+
+    voice_state *voiceB = &voices[3];
+    voiceB->phase_accum += voiceB->phase_step;
+    // 32 accumulator bits - 7 wavetable bits
+    uint8_t phaseB = voiceB->phase_accum >> (32-N);
+    voiceB->output = ((wavetableA[phaseB]) * lfo);
+    voiceB->output += ((wavetableB[phaseB]) * (127-lfo));
+    //output = filter1pole_feed(&filter, (eg>>4), output);
+    voiceB->output = (voiceB->output>>8) * (voiceB->amp);
+
+    pwm_channel2.write(voiceA->output);
+    pwm_channel2.write(voiceB->output);
   }
 }
 
@@ -121,11 +148,16 @@ void handleNoteOn(byte channel, byte pitch, byte velocity)
   Serial.print(" velocity = ");
   Serial.println(velocity);
 
-  osc *voice = &voices[0];
+ 
+  voice_state *voice = &voices[voice_allocation[voice_alloc_head]];
+  voice_alloc_head++;
+  voice_alloc_head %= 4;
+
   voice->note = pitch;
   voice->freq = midiNoteFreq[pitch];
+  voice->gate = true;
+  voice->amp = (uint8_t)velocity<<4;
   set_phase_step(voice,voice->freq);
-  eg = (int)velocity<<4;
 }
 
 void handleNoteOff(byte channel, byte pitch, byte velocity)
@@ -150,7 +182,7 @@ void handleCC(byte channel, byte control, byte value)
     load_wavetable(wavetableB, (int)value%28,true);
   }
   if (control == 1) {
-    modulation = (int) value;
+    modulation_step = (int) value;
   }
 }
 
@@ -180,15 +212,20 @@ void setup() {
   }
   load_wavetable(wavetableA, wta_sel,true);
   load_wavetable(wavetableB, wtb_sel);
-  pwm.setBuffers(4, 32); // Give larger buffers since we're are 48khz sample rate
-  pwm.onTransmit(cb);
-  pwm.begin(freq);
+
+  pwm_channel1.setBuffers(4, 32); // Give larger buffers since we're are 48khz sample rate
+  pwm_channel1.onTransmit(channel1_cb);
+  pwm_channel1.begin(freq);
+
+  pwm_channel2.setBuffers(4, 32); // Give larger buffers since we're are 48khz sample rate
+  pwm_channel2.onTransmit(channel2_cb);
+  pwm_channel2.begin(freq);
 
   // wait until device mounted
   while( !TinyUSBDevice.mounted() ) delay(1);
 }
 
-void set_phase_step(osc *voice, int freqout) {
+void set_phase_step(voice_state *voice, int freqout) {
   voice->phase_step = freqout * base_freq;
 }
 
@@ -197,9 +234,12 @@ void loop() {
   //Serial.println("working...");
   MIDI.read(); 
 
-  delay(10);
-  if (eg>0) {
-    eg -= 2;
+  delay(5);
+  for (int i = 0; i < 8; i++) {
+    voice_state *voice = &voices[i];
+    if (voice->amp > 0) {
+      voice->amp--;
+    }
   }
 }
 
